679eec522d8d95ad73093d55d62599bba9c0469c
Shunsuke Tadokoro <s.tadokoro0317@gmail.com>
2024-07-24 12:45:06 +0900

    fix

--- init.el
+++ init.el
@@ -28,27 +28,18 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(package-selected-packages
+   '(jtsx vertico-prescient vertico-posframe typescript-mode treesit-auto tempel symbol-overlay spacious-padding rainbow-delimiters quelpa-use-package puni pulsar perfect-margin org-modern orderless nerd-icons-dired nerd-icons-completion mermaid-mode marginalia goggles fontaine exec-path-from-shell emojify embark-consult eldoc-box eglot-signature-eldoc-talkative eglot-booster ef-themes difftastic diff-hl corfu-prescient copilot consult-eglot cape breadcrumb blackout aggressive-indent)))
 (custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(fringe ((t :background "white")))
- '(header-line ((t :box (:line-width 4 :color "grey90" :style nil))))
- '(header-line-highlight ((t :box (:color "black"))))
- '(keycast-key ((t)))
- '(line-number ((t :background "white")))
- '(mode-line ((t (:inherit ef-themes-ui-variable-pitch :background "#111111" :foreground "#d0ffe0" :box (:line-width (2 . 2) :color "#111111") :overline "#d0ffe0" :height 0.9))))
- '(mode-line-active ((t :background "white" :overline "black" :box (:line-width 6 :color "white" :style nil))))
- '(mode-line-highlight ((t :box (:color "black"))))
- '(mode-line-inactive ((t :background "white" :overline "grey20" :box (:line-width 6 :color "white" :style nil))))
- '(tab-bar-tab ((t :box (:line-width 4 :color "grey85" :style nil))))
- '(tab-bar-tab-inactive ((t :box (:line-width 4 :color "grey75" :style nil))))
- '(tab-line-tab ((t)))
- '(tab-line-tab-active ((t)))
- '(tab-line-tab-inactive ((t)))
- '(vertical-border ((t :background "white" :foreground "white")))
- '(window-divider ((t (:background "white" :foreground "white"))))
- '(window-divider-first-pixel ((t (:background "white" :foreground "white"))))
- '(window-divider-last-pixel ((t (:background "white" :foreground "white")))))
+        ;; custom-set-faces was added by Custom.
+        ;; If you edit it by hand, you could mess it up, so be careful.
+        ;; Your init file should contain only one such instance.
+        ;; If there is more than one, they won't work right.
+        '(fringe nil)
+        '(header-line nil)
+        '(header-line-highlight nil)
+        '(keycast-key ((t)))
+        '(line-number ((t :background "gray100")))
+        '(mode-line ((t :background nil :overline "SeaGreen4")))
+        '(mode-line-active ((t :background nil :overline "SeaGreen4")))
+        '(mode-line-inactive ((t :overline "gray40"))))
