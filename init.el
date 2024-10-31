6bde1d85a0bb8c506227ff48260978d9d7bd5ebb
shunsuke.tadokoro <shunsuke.tadokoro@bizreach.co.jp>
2024-10-31 11:02:40 +0900

    refine

--- init.el
+++ init.el
@@ -30,8 +30,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-	  '(sbt-mode scala-mode go-mode jtsx vertico-prescient vertico-posframe typescript-mode treesit-auto tempel symbol-overlay spacious-padding rainbow-delimiters quelpa-use-package puni pulsar perfect-margin org-modern orderless nerd-icons-dired nerd-icons-completion mermaid-mode marginalia goggles fontaine exec-path-from-shell emojify embark-consult eldoc-box eglot-signature-eldoc-talkative eglot-booster ef-themes difftastic diff-hl corfu-prescient copilot consult-eglot cape breadcrumb blackout aggressive-indent))
- '(tab-width 4))
+   '(go-mode markdown-mode treemacs-magit treemacs-all-the-icons treemacs-projectile treemacs projectile jtsx vertico-prescient vertico-posframe typescript-mode treesit-auto tempel symbol-overlay spacious-padding rainbow-delimiters quelpa-use-package puni pulsar perfect-margin org-modern orderless nerd-icons-dired nerd-icons-completion mermaid-mode marginalia goggles fontaine exec-path-from-shell emojify embark-consult eldoc-box eglot-signature-eldoc-talkative eglot-booster ef-themes difftastic diff-hl corfu-prescient copilot consult-eglot cape breadcrumb blackout aggressive-indent)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
